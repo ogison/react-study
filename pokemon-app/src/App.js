5207cb1198e97427ad46e7c4b3a621d9dabeebef
ogison <meyo.ogs9@gmail.com>
2024-06-02 22:24:34 +0900

    Init react-study

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,20 @@
+import {useEffect} from "react";
+import './App.css';
+import { getAllPokemon } from "./utils/pokemon.js";
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() =>{
+    const fetchPokemonData = async () =>{
+      // すべてのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+  
+  return <div className="App"></div>;
+}
+
+export default App;
